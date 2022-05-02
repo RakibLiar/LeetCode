998ecddb2d354584ff61e4e1bc1dddd6084d6eef
RakibLiar <liar.coder@gmail.com>
2022-05-03 00:30:22 +0600

    Time: 17 ms (35.77%), Space: 16.1 MB (83.25%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -3,17 +3,15 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int low = 0, high = nums.size()-1;
         while(low<high) {
-            if(nums[low]%2 == 0)
-                low++;
-            if(nums[high]%2)
-                high--;
-            if(low>=high)
-                break;
             if(nums[low]%2 == 1 && nums[high]%2 == 0) {
                 swap(nums[low], nums[high]);
                 low++;
                 high--;
             }
+            if(nums[low]%2 == 0)
+                low++;
+            if(nums[high]%2)
+                high--;
         }
         return nums;
     }
