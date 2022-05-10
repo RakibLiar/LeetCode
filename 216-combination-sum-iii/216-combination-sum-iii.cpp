bc8478b172e63450b67816f278e2facdd8bc62cc
RakibLiar <liar.coder@gmail.com>
2022-05-10 08:20:49 +0600

    Time: 7 ms (12.52%), Space: 7.1 MB (14.34%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -9,10 +9,10 @@ public:
         }
         for(int i=pos+1; i<=9; i++) {
             if(!digit[i] && n-i >= 0) {
-                digit[i] = true;
+                //digit[i] = true;
                 v.push_back(i);
                 calculateResult(v, digit, k, n-i, i);
-                digit[i] = false;
+                //digit[i] = false;
                 v.pop_back();
             }
         }
