c0af7e17b04b3364538a8c3a7ad763379a5cd240
RakibLiar <liar.coder@gmail.com>
2022-05-10 08:22:42 +0600

    Time: 0 ms (100.00%), Space: 6.7 MB (37.26%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<vector<int>> result;
     void calculateResult(vector<int> v, int k, int n, int pos) {
+        if(n<0) return;
         if(v.size() == k) {
             if(n==0) result.push_back(v);
             return;
         }
         for(int i=pos+1; i<=9; i++) {
-            if(n-i >= 0) {
-                v.push_back(i);
-                calculateResult(v, k, n-i, i);
-                v.pop_back();
-            }
+            v.push_back(i);
+            calculateResult(v, k, n-i, i);
+            v.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
