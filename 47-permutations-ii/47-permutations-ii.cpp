0cca5880842740d3547f1a710af993eaed0a5558
RakibLiar <liar.coder@gmail.com>
2022-05-12 16:20:31 +0600

    Time: 950 ms (5.00%), Space: 123.1 MB (5.04%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    set<vector<int>> st;
-    void getPermutation(vector<int> nums, vector<int> v, vector<bool> visited, int pos) {
+    unordered_map<long long, bool> ump;
+    vector<vector<int>> result;
+    int mod = 1e9 + 7, base = 37;
+    void getPermutation(vector<int> nums, vector<int> v, vector<bool> visited, int pos, long long hash) {
         if(pos == nums.size()) {
-            st.insert(v);
+            if(!ump[hash]) {
+                result.push_back(v);
+                ump[hash] = true;
+            }
             return;
         }
         for(int i=0; i<nums.size(); i++) {
             if(!visited[i]) {
+                auto temp = hash * base + nums[i];
+                temp = temp%mod;
                 visited[i] = true;
-                v.push_back(nums[i]);
-                getPermutation(nums, v, visited, pos+1);
+                v.push_back(nums[i]-10);
+                getPermutation(nums, v, visited, pos+1, temp);
                 visited[i] = false;
                 v.pop_back();
             }
@@ -19,11 +26,10 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<bool> visited(nums.size(), false);
         vector<int> v;
-        getPermutation(nums, v, visited, 0);
-        vector<vector<int>> result;
-        for(auto it: st) {
-            result.push_back(it);
+        for(int i=0; i<nums.size(); i++) {
+            nums[i] += 10;
         }
+        getPermutation(nums, v, visited, 0, 0 * 1ll);
         return result;
     }
 };
