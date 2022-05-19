b68655fce493e55db1058a579cf93d337bdc2071
RakibLiar <liar.coder@gmail.com>
2022-05-20 01:48:07 +0600

    Time: 143 ms (20.37%), Space: 24.9 MB (21.37%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& matrix, int r, int c) {
-        return 0<=r && r<matrix.size() && 0<=c && c<matrix[0].size();
-    }
-    void bfs(vector<vector<int>> &level, vector<vector<int>> &matrix, int x_pos, int y_pos) {
-        queue<pair<int, int>> q;
-        q.push({x_pos, y_pos});
-        level[x_pos][y_pos] = 1;
-        vector<int> dir {0,1,0,-1,0};
-        while(!q.empty()) {
-            auto par = q.front();
-            q.pop();
-            for(int i=0; i<dir.size()-1; i++) {
-                int x = par.first + dir[i];
-                int y = par.second + dir[i+1];
-                if(check(matrix, x, y) && matrix[par.first][par.second] > matrix[x][y] && level[par.first][par.second] + 1 > level[x][y]) {
-                    level[x][y] = level[par.first][par.second]+1;
-                    q.push({x, y});
-                }
-            }
+    int dfs(vector<vector<int>> &dp, vector<vector<int>> &matrix, int x, int y, int prev) {
+        if(x<0 || x>=matrix.size() || y<0 || y>=matrix[0].size() || matrix[x][y] <= prev) return 0;
+        if(dp[x][y] != -1) return dp[x][y];
+        int best = 0;
+        vector<int> dir{0,1,0,-1,0};
+        for(int i=0; i<dir.size()-1; i++) {
+            int dx = x + dir[i];
+            int dy = y + dir[i+1];
+            best = max(best, dfs(dp, matrix, dx, dy, matrix[x][y]));
         }
+        return dp[x][y] = best + 1;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int row = matrix.size(), col = matrix[0].size();
-        vector<vector<int>> level(row, vector<int>(col, 0));
+        vector<vector<int>> dp(row, vector<int>(col, -1));
+        int answer = INT_MIN;
         for(int i=0; i<matrix.size(); i++) {
             for(int j=0; j<matrix[0].size(); j++) {
-                if(level[i][j] == 0) {
-                    bfs(level, matrix, i, j);
-                }
+                answer = max(answer, dfs(dp, matrix, i, j, INT_MIN));
             }
         }
-        int ans = INT_MIN;
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++)
-                ans = max(ans, level[i][j]);
-        }
-        return ans;
+        return answer;
     }
 };
