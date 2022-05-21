e90eeae118cdb80ec0ba4654e16146b633062076
RakibLiar <liar.coder@gmail.com>
2022-05-22 05:00:45 +0600

    Time: 305 ms (8.48%), Space: 89.6 MB (5.12%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int calculate(vector<vector<int>> &dp, vector<int> &coins, int amount, int pos, int cnt) {
+    int calculate(vector<vector<int>> &dp, vector<int> &coins, int amount, int pos) {
         if(amount<0) return INT_MAX-100;
         if(amount == 0) {
             return 0;
@@ -11,14 +11,14 @@ public:
         if(dp[amount][pos] != -1) return dp[amount][pos];
         int ans1 = INT_MAX, ans2 = INT_MAX;
         if(amount>=coins[pos])
-            ans1 = calculate(dp, coins, amount-coins[pos], pos, cnt+1) + 1;
-        ans2 = calculate(dp, coins, amount, pos+1, cnt);
+            ans1 = calculate(dp, coins, amount-coins[pos], pos) + 1;
+        ans2 = calculate(dp, coins, amount, pos+1);
         return dp[amount][pos] = min(ans1, ans2);
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(amount+1, vector<int> (coins.size(), -1));
         sort(coins.begin(), coins.end());
-        int ans = calculate(dp, coins, amount, 0, 0);
+        int ans = calculate(dp, coins, amount, 0);
         return ans >= INT_MAX - 100 ? -1 : ans;
     }
 };
