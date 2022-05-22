cae9e34a7e2c7e54238972dd17249ba370de2650
RakibLiar <liar.coder@gmail.com>
2022-05-23 04:02:58 +0600

    Time: 7 ms (72.39%), Space: 6.2 MB (96.66%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool calculate(vector<vector<bool>> &dp, string s, int pos1, int pos2) {
-        if(pos1>=pos2) return true;
-        if(dp[pos1][pos2]) return true;
-        if(s[pos1] != s[pos2])
-            return dp[pos1][pos2] = false;
-        return dp[pos1][pos2] = calculate(dp, s, pos1+1, pos2-1);
-    }
     int countSubstrings(string s) {
-        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
-        int cnt = 0;
-        for(int i=0; i<s.size(); i++) {
-            for(int j=i; j<s.size(); j++) {
-                if(dp[i][j] || calculate(dp, s, i, j)) {
-                    cnt++;
-                }
+        int i,j,k,ans=0, len = s.size();
+        string result;
+        for(k=0;k<len;k++)
+        {
+            for(i=k,j=k;i>=0 && j<len;i--,j++)
+            {
+                if(s[i]!=s[j])
+                    break;
+                ans++;
+            }
+            for(i=k,j=k+1;i>=0 && j<len;i--,j++)
+            {
+                if(s[i]!=s[j])
+                    break;
+                ans++;
             }
         }
-        return cnt;
+        return ans;
     }
 };
