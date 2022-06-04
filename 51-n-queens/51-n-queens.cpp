fe710302880cdc124e5068e8da6781b130bb5d9d
RakibLiar <liar.coder@gmail.com>
2022-06-05 02:34:41 +0600

    Time: 3 ms (96.12%), Space: 7.3 MB (67.78%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<string>> result;
+    bool hasQueen(vector<pair<int, int>> &pos, int row, int col) {
+        for(auto p: pos) {
+            if(p.first == row || p.second == col)
+                return true;
+            if(abs(p.first-row) == abs(p.second-col))
+                return true;
+        }
+        return false;
+    }
+    void setQueens(int row, vector<pair<int, int>> &pos, int n, vector<string> &vec) {
+        if(row == n) {
+            int cnt = 0;
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(vec[i][j] == 'Q')
+                        cnt++;
+                }
+            }
+            if(cnt == n)
+                result.push_back(vec);
+            return;
+        }
+        for(int i=0; i<n; i++) {
+            if(!hasQueen(pos, row, i)) {
+                vec[row][i] = 'Q';
+                pos.push_back({row, i});
+                setQueens(row+1, pos, n, vec);
+                vec[row][i] = '.';
+                pos.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> vec;
+        vector<pair<int, int>> pos;
+        string dot;
+        for(int i=0; i<n; i++)
+            dot.push_back('.');
+        for(int i=0; i<n; i++)
+            vec.push_back(dot);
+        setQueens(0, pos, n, vec);
+        return result;
+    }
+};
