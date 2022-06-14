f541df74f197f6697531d85fddbb84ef9f6ecaec
RakibLiar <liar.coder@gmail.com>
2022-06-14 23:37:14 +0600

    Time: 67 ms (16.13%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int calculate(string word1, string word2, vector<vector<int>> &dp, int pos1, int pos2) {
-        if(pos1>=word1.size() || pos2>=word2.size())
-            return 0;
-        if(dp[pos1][pos2] != -1) return dp[pos1][pos2];
-        int res1 = 0, res2 = 0;
-        if(word1[pos1] == word2[pos2])
-            res1 = calculate(word1, word2, dp, pos1+1, pos2+1) + 1;
-        else
-            res2 = max(calculate(word1, word2, dp, pos1+1, pos2), calculate(word1, word2, dp, pos1, pos2+1));
-        return dp[pos1][pos2] = max(res1, res2);
+    int calculate(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
-        int a = calculate(word1, word2, dp, 0, 0);
+        int a = calculate(word1, word2);
         return word1.size() + word2.size() - 2*a;
     }
 };
