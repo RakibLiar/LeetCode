f9fb92c40f905cd5ad2dca5b74da29110056d491
RakibLiar <liar.coder@gmail.com>
2022-06-20 03:49:16 +0600

    Time: 31 ms (56.53%), Space: 13 MB (40.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int lcs(string text1, string text2, int pos1, int pos2) {
-        if(pos1 == text1.size() || pos2 == text2.size()) return 0;
-        if(dp[pos1][pos2] != -1) return dp[pos1][pos2];
-        if(text1[pos1] == text2[pos2])
-            return dp[pos1][pos2] = lcs(text1, text2, pos1+1, pos2+1) + 1;
-        return dp[pos1][pos2] = max(lcs(text1, text2, pos1+1, pos2), lcs(text1, text2, pos1, pos2+1));
-    }
-    
     int longestCommonSubsequence(string text1, string text2) {
-        dp = vector<vector<int>>(text1.size()+1, vector<int>(text2.size()+1, 0));
+        vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1, 0));
         for(int i=0; i<=text1.size(); i++) {
             for(int j=0; j<=text2.size(); j++) {
                 if(i==0 || j==0)
