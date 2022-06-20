8a6ac6dcb45da79ca894c815be49323d347c90cb
RakibLiar <liar.coder@gmail.com>
2022-06-21 00:19:50 +0600

    Time: 500 ms (6.44%), Space: 14.6 MB (88.61%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        unordered_map<string, bool> mp, res;
-        set<string> st;
-        for(string word: words) {
-            st.insert(word);
-        }
-        for(string word: words) {
-            string key;
-            int n = word.size();
-            for(int i=word.size()-1; i>0; i--) {
-                key = word.substr(i, n-i);
-                if(st.find(key) != st.end())
-                    st.erase(key);
+        sort(words.begin(), words.end(), [](string &a, string &b) {
+            return a.size() > b.size();
+        });
+        string ans;
+        for(string s: words) {
+            if(ans.find(s + "#") == string::npos) {
+                ans += (s + "#");
             }
         }
-        int ans = 0;
-        for(auto s: st) {
-            ans += (s.size() + 1);
-        }
-        return ans;
+        return ans.size();
     }
 };
