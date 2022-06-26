214497b9f890cb68ec631af2facc863ffb59b3dc
RakibLiar <liar.coder@gmail.com>
2022-06-27 01:15:13 +0600

    Time: 103 ms (30.97%), Space: 42.5 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -7,7 +7,8 @@ public:
         res = min(res, sum);
         for(int i=n-k; i<n; i++) {
             sum += (-nums[i-n+k] + nums[i]);
-            res = min(res, sum);
+            if(res > sum)
+                res = sum;
         }
         sum = 0;
         for(int a: nums) sum += a;
