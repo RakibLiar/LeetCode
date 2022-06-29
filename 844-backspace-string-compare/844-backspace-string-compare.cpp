7d8021eb4cf3fcf8d58207aed595d7c744f2814d
RakibLiar <liar.coder@gmail.com>
2022-06-29 15:57:09 +0600

    Time: 3 ms (49.60%), Space: 6.3 MB (67.03%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        vector<char> v, v2;
-        for(auto c:s) {
-            if(v.size() && c == '#')
-                v.pop_back();
+        vector<char> v1,v2;
+        for(auto c: s) {
+            if(c == '#' && !v1.empty())
+                v1.pop_back();
             else if(c != '#')
-                v.push_back(c);
+                v1.push_back(c);
         }
-        for(auto c:t) {
-            if(v2.size() && c == '#')
+        for(auto c: t) {
+            if(c == '#' && !v2.empty())
                 v2.pop_back();
             else if(c != '#')
                 v2.push_back(c);
         }
-        return v == v2;
+        return v1 == v2;
     }
 };
