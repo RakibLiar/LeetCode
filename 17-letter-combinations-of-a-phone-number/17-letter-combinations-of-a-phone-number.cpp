d16dcdb9a7fcdac99e4bbe5e03d4f360ebc3f990
RakibLiar <liar.coder@gmail.com>
2022-06-30 00:49:04 +0600

    Time: 0 ms (100.00%), Space: 6.4 MB (84.93%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<string> result;
-    vector<string> numbers{"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void letterResult(int pos, int sz, string digits, string res="") {
-        if(pos == sz) {
-            result.push_back(res);
-            return;
+    vector<string> digit_map{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> cartisianProduct(vector<string> &v, string s) {
+        vector<string> res;
+        for(auto s1: v) {
+            for(auto c: s) {
+                res.push_back(s1+c);
+            }
         }
-        int digit = digits[pos]-'0';
-        for(int i=0; i<numbers[digit].size(); i++) {
-            letterResult(pos+1, sz, digits, res+numbers[digit][i]);
-        }
+        return res;
     }
-    vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0) return result;
-        letterResult(0,digits.size(),digits);
-        return result;
+    vector<string> letterCombinations(string digits, int pos = 0) {
+        if(!digits.size()) return {};
+        vector<string> res{""};
+        for(auto c: digits) {
+            res = cartisianProduct(res, digit_map[c-'0']);
+        }
+        return res;
     }
 };
