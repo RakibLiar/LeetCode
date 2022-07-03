da985d41adb15d257d26233b5913ad433dcbb700
RakibLiar <liar.coder@gmail.com>
2022-07-04 04:56:22 +0600

    Time: 4 ms (44.11%), Space: 8.1 MB (9.73%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int calculate(string s, string cur, int pos, vector<int> &dp) {
+        if(cur.size() && (cur[0] == '0' || stoi(cur) > 26)) return 0;
+        if(s.size() == 1) return s != "0";
+        if(pos >= s.size()) return 1;
+        if(dp[pos] != -1) return dp[pos];
+        cur = "";
+        if(pos <= s.size()-2) {
+            dp[pos] = calculate(s, cur + s[pos], pos+1, dp) + calculate(s, cur + s[pos] + s[pos+1], pos+2, dp);
+        } else {
+            dp[pos] = calculate(s, cur + s[pos], pos+1, dp);
+        }
+        return dp[pos];
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1);
+        return calculate(s, "", 0, dp);
+    }
+};
