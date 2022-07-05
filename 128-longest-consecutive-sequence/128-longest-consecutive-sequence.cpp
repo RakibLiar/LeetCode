77865b00fc74eb7c4e7630d5ab2b1538ea5452c0
RakibLiar <liar.coder@gmail.com>
2022-07-05 18:55:52 +0600

    Time: 244 ms (56.36%), Space: 51.2 MB (25.30%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -6,16 +6,16 @@ public:
         for(int n: nums) {
             mp[n] = true;
         }
-        auto it = mp.begin(), prev = mp.begin();
-        int ans = 0, cnt = 1;
+        auto it = mp.begin();
+        int ans = 0, cnt = 1, prev = mp.begin()->first;
         for(++it; it != mp.end(); it++) {
-            if(it->first == prev->first + 1) {
+            if(it->first == prev + 1) {
                 cnt++;
             } else {
                 ans = max(cnt, ans);
                 cnt = 1;
             }
-            prev = it;
+            prev = it->first;
         }
         return max(ans, cnt);
     }
