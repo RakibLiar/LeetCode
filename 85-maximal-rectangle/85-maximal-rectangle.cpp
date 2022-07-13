b2198f3b986d44e6df381ef270ced95cc386df66
RakibLiar <liar.coder@gmail.com>
2022-07-14 02:38:00 +0600

    Time: 47 ms (91.74%), Space: 14.8 MB (47.28%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(vector<int> &nums) {
+        stack<pair<int,int>> st;
+        st.push({-1, -1});
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++) {
+            while(st.top().first >= nums[i]) {
+                auto t = st.top();
+                st.pop();
+                ans = max(ans, (i-st.top().second-1)*t.first);
+            }
+            st.push({nums[i], i});
+        }
+        while(st.size()>1) {
+            auto t = st.top();
+            st.pop();
+            ans = max(ans, (int)(nums.size() - st.top().second-1)*t.first);
+        }
+        return ans;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int row = matrix.size(), col = matrix[0].size();
+        vector<vector<int>>nums(row, vector<int>(col, 0));
+        for(int i=0; i<col; i++) nums[0][i] = matrix[0][i] - '0';
+        for(int i=1; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(matrix[i][j] == '1') {
+                    nums[i][j] += (nums[i-1][j] + 1);
+                }
+            }
+        }
+        int ans = 0;
+        for(auto v: nums) {
+            ans = max(ans, calculate(v));
+        }
+        return ans;
+    }
+};
