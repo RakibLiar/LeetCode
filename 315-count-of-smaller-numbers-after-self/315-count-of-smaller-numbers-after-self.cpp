181a9c89a74c476129e940816f0fce38cbee10a5
RakibLiar <liar.coder@gmail.com>
2022-07-23 14:40:11 +0600

    Time: 632 ms (63.31%), Space: 99 MB (63.53%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int>tree;
+    int query(int node, int b, int e, int i, int j)
+    {
+        if (i > e || j < b)
+            return 0;
+        if (b >= i && e <= j)
+            return tree[node];
+        int Left = node * 2;
+        int Right = node * 2 + 1;
+        int mid = (b + e) / 2;
+        int p1 = query(Left, b, mid, i, j);
+        int p2 = query(Right, mid + 1, e, i, j);
+        return p1 + p2;
+    }
+    void update(int node, int b, int e, int i, int newvalue)
+    {
+        if (i > e || i < b)
+            return;
+        if (b >= i && e <= i) {
+            tree[node] += newvalue;
+            return;
+        }
+        int Left = node * 2;
+        int Right = node * 2 + 1;
+        int mid = (b + e) / 2;
+        update(Left, b, mid, i, newvalue);
+        update(Right, mid + 1, e, i, newvalue);
+        tree[node] = tree[Left] + tree[Right];
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+        tree = vector<int>(100100, 0);
+        vector<int> res(nums.size());
+        for(int i=nums.size()-1; i>=0; i--) {
+            int a = nums[i] + 10001;
+            update(1, 1, 20002, a, 1);
+            int b = query(1,1,20002,1 ,a-1);
+            res[i] = b;
+        }
+        return res;
+    }
+};
