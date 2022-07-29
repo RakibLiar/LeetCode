bdedfb59c6484663ee4f18616866a63bcf0f4f71
RakibLiar <liar.coder@gmail.com>
2022-07-30 05:34:13 +0600

    Time: 53 ms (96.30%), Space: 39.5 MB (26.13%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        vector<int> v(values.size());
-        for(int i=0; i<values.size(); i++) {
-            v[i] = values[i]+i;
-        }
         int ans = INT_MIN, maxValue = values[0];
         for(int i=1; i<values.size(); i++) {
             ans = max(ans, maxValue + values[i]-i);
