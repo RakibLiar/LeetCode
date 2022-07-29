a9bdc829ed1e74af58bf5e5dbda345427a08af07
RakibLiar <liar.coder@gmail.com>
2022-07-29 20:07:26 +0600

    Time: 7 ms (55.39%), Space: 8.6 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    void precalculate(vector<int> &vec, string &s) {
-        int seq = 0;
-        vector<int> v;
-        for(char c: s) {
-            if(vec[c-'a'] == -1) {
-                vec[c-'a'] = seq++;
-            }
-        }
-        for(int a: vec) {
-            if(a != -1)
-                v.push_back(a);
-        }
-        vec = v;
-    }
     bool canAdd(string s, string p) {
         vector<char> v1(200, 0), v2(200, 0);
         for(int i=0; i<s.size(); i++) {
@@ -32,10 +18,7 @@ public:
         return true;
     }
     vector<string> findAndReplacePattern(vector<string>& words, string p) {
-        vector<int> pat(26, -1);
         vector<string> res;
-        vector<char> vc(200, 0);
-        precalculate(pat, p);
         for(string s: words) {
             if(canAdd(s, p)) {
                 res.push_back(s);
