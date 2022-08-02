50ed37b93411c4c73de5c515aa4e9ed1ec103623
RakibLiar <liar.coder@gmail.com>
2022-08-03 00:38:59 +0600

    Time: 51 ms (56.64%), Space: 14.4 MB (33.33%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -5,6 +5,8 @@ public:
         priority_queue<int> pq;
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
+                if(pq.size() >= k && matrix[i][j] >= pq.top())
+                    break;
                 pq.push(matrix[i][j]);
                 if(pq.size()>k)
                     pq.pop();
