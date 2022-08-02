498d48fdf8676696f1c9ef8abbfeab0f3f86a657
RakibLiar <liar.coder@gmail.com>
2022-08-03 02:11:18 +0600

    Time: 215 ms (5.03%), Space: 20.9 MB (5.69%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
+    bool check(int x, int y, int n) {
+        return 0<=x and x<n and 0<=y and y<n;
+    }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int row = matrix.size(), col = matrix[0].size();
-        priority_queue<int> pq;
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-                if(pq.size() >= k && matrix[i][j] >= pq.top())
-                    break;
-                pq.push(matrix[i][j]);
-                if(pq.size()>k)
-                    pq.pop();
+        int n = matrix.size(), cnt = 0, ans = matrix[0][0];
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({matrix[0][0], 0, 0});
+        vector<vector<bool>> test(n, vector<bool>(n, false));
+        while(cnt < k) {
+            vector<int> v = pq.top();
+            pq.pop();
+            cnt++;
+            //cout<<cnt<<" "<<v[0]<<endl;
+            if(cnt == k) {
+                ans = v[0];
+                break;
+            }
+            int x = v[1], y = v[2];
+            if(check(x+1, y, n) && !test[x+1][y]) {
+                pq.push({matrix[x+1][y], x+1, y});
+                test[x+1][y] = true;
+            }
+            if(check(x, y+1, n) && !test[x][y+1]) {
+                pq.push({matrix[x][y+1], x, y+1});
+                test[x][y+1] = true;
             }
         }
-        return pq.top();
+        return ans;
     }
 };
