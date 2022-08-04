2fe1c416ee4309019225531b6faf7a1ac9033be9
RakibLiar <liar.coder@gmail.com>
2022-08-05 02:15:44 +0600

    Time: 5 ms (12.40%), Space: 5.9 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        if(b == 0) return a;
-        return gcd(b, a%b);
-    }
     int mirrorReflection(int p, int q) {
-        int g = gcd(p,q);
+        int g = __gcd(p,q);
         if((p/g)%2 == 0) return 2;
         return (q/g)%2;
     }
