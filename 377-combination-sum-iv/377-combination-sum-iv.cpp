b15da638aed54fa4211c5726efb58cbbf98e74db
RakibLiar <liar.coder@gmail.com>
2022-08-06 01:04:47 +0600

    Time: 45 ms (5.03%), Space: 91.8 MB (5.01%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int calculate(vector<int> &nums, int pos, int sum) {
+    Solution() {
+        dp = vector<vector<int>>(1001, vector<int>(1001, -1));
+    }
+    int combinationSum4(vector<int>& nums, int sum, int pos = 0) {
         if(sum < 0) return 0;
         if(sum == 0) return 1;
         if(dp[pos][sum] != -1) return dp[pos][sum];
         int a = 0;
         for(int i=0; i<nums.size(); i++) {
-            a += calculate(nums, i, sum-nums[i]);
+            a += combinationSum4(nums, sum-nums[i], i);
         }
         return dp[pos][sum] = a;
-    }
-    int combinationSum4(vector<int>& nums, int target) {
-        dp = vector<vector<int>>(nums.size(), vector<int>(target+1, -1));
-        return calculate(nums, 0, target);
     }
 };
