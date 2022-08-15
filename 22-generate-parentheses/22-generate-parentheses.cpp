260c453bc91c07573b12e786614cde5e8f888418
RakibLiar <liar.coder@gmail.com>
2022-08-15 16:46:54 +0600

    Time: 12 ms (24.14%), Space: 15.1 MB (27.51%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     vector<string> res;
     void generate(int n, int first, int second, string s) {
+        if(first > n || second > n || second > first) return;
         if(first == n && second == n) {
             res.push_back(s);
             return;
         }
-        if(first > n || second > n) return;
-        if(second > first) return;
-        if(first<n) {
-            generate(n, first+1, second, s+"(");
-        }
-        if(second<n) {
-            generate(n, first, second+1, s+")");
-        }
+        generate(n, first+1, second, s+"(");
+        generate(n, first, second+1, s+")");
     }
     vector<string> generateParenthesis(int n) {
         generate(n, 0, 0, "");
