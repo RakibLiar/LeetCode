deacc9ee5c0779cd0ba0a503516934e2cac0756e
RakibLiar <liar.coder@gmail.com>
2022-08-18 00:37:37 +0600

    Time: 0 ms (100.00%), Space: 7.8 MB (25.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int calculate(vector<int> &nums, vector<vector<int>> &dp, int prev, int pos) {
-        if(pos == nums.size()) return 0;
-        if(dp[prev][pos] != -1) return dp[prev][pos];
-        if(prev == 1) {
-            return dp[prev][pos] = calculate(nums, dp, 0, pos+1);
-        }
-        return dp[prev][pos] = max(calculate(nums, dp, 1, pos+1) + nums[pos], calculate(nums, dp, 0, pos+1));
+    int calculate(vector<int> &nums, vector<int> &dp, int pos) {
+        if(pos >= nums.size()) return 0;
+        if(dp[pos] != -1) return dp[pos];
+        return dp[pos] = max(calculate(nums, dp, pos+2) + nums[pos], calculate(nums, dp, pos+1));
     }
     int rob(vector<int>& nums) {
-        vector<vector<int>> dp(2, vector<int>(nums.size(), -1));
-        return max(calculate(nums, dp, 0, 0), calculate(nums, dp, 1, 0));
+        vector<int> dp(nums.size(), -1);
+        return calculate(nums, dp, 0);
     }
 };
