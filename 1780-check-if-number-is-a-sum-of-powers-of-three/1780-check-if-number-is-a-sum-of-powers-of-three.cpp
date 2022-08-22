afc4fc5a63ec227388429ceb395f1098715741c3
RakibLiar <liar.coder@gmail.com>
2022-08-22 22:50:22 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (77.62%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        while(n != 0 && n%3 <= 1) {
-            n/=3;
-        }
+        while(n != 0 && n%3 <= 1) n/=3;
         return n == 0;
     }
 };
