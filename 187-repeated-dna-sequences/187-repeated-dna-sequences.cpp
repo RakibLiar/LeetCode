43a6c987a27fe58751b0190b218d43297d46ae20
RakibLiar <liar.coder@gmail.com>
2022-08-27 03:36:30 +0600

    Time: 71 ms (82.18%), Space: 23.5 MB (8.55%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        unordered_map<int, int> hasBefore;
+        unordered_map<string, int> hasBefore;
         vector<string> v;
         for(int i=0; i<=(int)s.size()-10; i++) {
-            int ans = 0;
-            for(int j=i; j<i+10; j++) {
-                ans = (5 * ans + (s[j]-'A'+1)%5);
-            }
             string str = s.substr(i, 10);
-            hasBefore[ans]++;
-            if(hasBefore[ans] == 2) {
+            if(++hasBefore[str] == 2) {
                 v.push_back(str);
             }
         }
