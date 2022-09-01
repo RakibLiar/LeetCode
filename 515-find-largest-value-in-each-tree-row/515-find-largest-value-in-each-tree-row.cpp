1f49a92f064b9e645bc3849c97f09edd37a2efa2
RakibLiar <liar.coder@gmail.com>
2022-09-02 04:02:12 +0600

    Time: 26 ms (28.21%), Space: 22 MB (87.36%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -11,22 +11,33 @@
  */
 class Solution {
 public:
+    void getResult(TreeNode *root, int level, vector<int> &v) {
+        if(root == NULL) return;
+        if(level == v.size()) v.push_back(root->val);
+        else v[level] = max(v[level], root->val);
+        getResult(root->left, level+1, v);
+        getResult(root->right, level+1, v);
+    }
+    
     vector<int> largestValues(TreeNode* root) {
-        if(root == NULL) return {};
-        queue<TreeNode*> q;
         vector<int> v;
-        q.push(root);
-        while(!q.empty()) {
-            int sz = q.size(), mx = INT_MIN;
-            for(int i=0; i<sz; i++) {
-                TreeNode *node = q.front();
-                q.pop();
-                mx = max(mx, node->val);
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
-            }
-            v.push_back(mx);
-        }
+        getResult(root, 0, v);
         return v;
+        // if(root == NULL) return {};
+        // queue<TreeNode*> q;
+        // vector<int> v;
+        // q.push(root);
+        // while(!q.empty()) {
+        //     int sz = q.size(), mx = INT_MIN;
+        //     for(int i=0; i<sz; i++) {
+        //         TreeNode *node = q.front();
+        //         q.pop();
+        //         mx = max(mx, node->val);
+        //         if(node->left != NULL) q.push(node->left);
+        //         if(node->right != NULL) q.push(node->right);
+        //     }
+        //     v.push_back(mx);
+        // }
+        // return v;
     }
 };
