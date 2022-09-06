b2936f3f2a1676e8642d5e29e24b45fe65dd858a
RakibLiar <liar.coder@gmail.com>
2022-09-06 16:18:17 +0600

    Time: 23 ms (46.83%), Space: 11.4 MB (53.08%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,31 +1,112 @@
-class Solution {
+class Node {
 public:
-    unordered_map<string, string> mp;
-    bool result = false;
-    int dp[1005];
-    int wordResult(string s, int pos) {
-        //cout<<tempString<<" "<<pos<<endl;
-        if(s.size() == pos) {
-            return 1;
-        }
-        if(dp[pos] != -1) return dp[pos];
-        string tempString = "";
-        int a = 0;
-        for(int i = pos; i<s.size(); i++) {
-            tempString += s[i];
-            if(mp.count(tempString)) {
-                a = a | wordResult(s, i+1);
-            }
-            //dp[pos] = dp[pos] | a;
-        }
-        return dp[pos] = a;
+    Node* link[26];
+    bool flag = false;
+ 
+    bool containKey(char ch)
+    {
+        return link[ch-'a'] != NULL;
     }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        for(int i=0; i<s.size()+2; i++)
-            dp[i] = -1;
-        for(auto it: wordDict) {
-            mp[it] = it;
+    void put(char ch, Node* node)
+    {
+        link[ch-'a'] = node;
+    }
+    Node* get(char ch)
+    {
+        return link[ch-'a'];
+    }
+    void setEnd()
+    {
+        flag = true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+ 
+class Trie {
+    Node* root;
+    int dp[300];
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+ 
+    Node* getRoot()
+    {
+        return root;
+    }
+    void insert(string word)
+    {
+        Node* temp = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!temp->containKey(word[i]))
+            {
+                temp->put(word[i], new Node());
+            }
+            temp = temp->get(word[i]);
         }
-        return wordResult(s, 0) == 1 ? true : false;
+        temp->setEnd();
+    }
+ 
+    // DP Memoization Approach
+    int search(string word, int startIdx)
+    {
+        Node* temp = root;
+        bool isFound = 0;
+        if (dp[startIdx] != -1)
+            return dp[startIdx];
+ 
+        for (int i = startIdx; i < word.size(); i++)
+        {
+            if (!temp->containKey(word[i]))
+            {
+                dp[startIdx] = 0;
+                return dp[startIdx];
+            }
+ 
+            temp = temp->get(word[i]);
+            if (temp->isEnd())
+            {
+                isFound = search(word, i+1);
+            }
+            if (isFound)
+                break;
+        }
+ 
+        return dp[startIdx] = (isFound || startIdx == word.size()) ? 1 : 0;
+ 
+    }
+ 
+    void resetMemoization(string s)
+    {
+        for (int i = 0; i < 300; i++)
+            dp[i] = -1;
+    }
+    void del(Node* cur)
+    {
+        for (int i = 0; i < 26; i++)
+            if (cur->link[i])
+                del(cur->link[i]);
+        delete (cur);
+    }
+};
+ 
+class Solution {
+ 
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Trie sol;
+        sol.resetMemoization(s);
+        for (int i = 0; i < wordDict.size(); i++)
+        {
+            sol.insert(wordDict[i]);
+        }        
+        bool ans = (sol.search(s, 0) == 1);
+        sol.del(sol.getRoot());
+        return ans;
     }
 };
