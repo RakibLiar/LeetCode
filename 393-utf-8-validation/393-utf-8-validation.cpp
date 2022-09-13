d7e7387c8c277af36514f7580214d2feaeefe185
RakibLiar <liar.coder@gmail.com>
2022-09-14 02:07:59 +0600

    Time: 38 ms (12.22%), Space: 14 MB (76.81%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string common(int n) {
+        string s;
+        for(int i=0; i<8; i++) {
+            s += (n%2 + '0');
+            n /= 2;
+        }
+        reverse(s.begin(), s.end());
+        //cout<<n<<" "<<s<<endl;
+        return s;
+    }
+    
+    bool isValid(int n) {
+        string s = common(n);
+        return s[0] == '1' && s[1] == '0';
+    }
+    
+    int utfBit(int n) {
+        string s = common(n);
+        int cnt = 0, i;
+        for(i=0; i<8 && s[i] == '1'; i++) {
+            if(s[i] == '1')
+                cnt++;
+        }
+        return (i != 8 && s[i] == '0') ? i : -1;
+    }
+    
+    bool validUtf8(vector<int>& data) {
+        int i;
+        for(i=0; i<data.size(); i++) {
+            int bit = utfBit(data[i]);
+            if(bit == 1 || bit == -1 || bit > 4) return false;
+            //cout<<i<<" "<<bit<<endl;
+            for(int j=1; j+i<data.size() && j < bit; j++) {
+                if(!isValid(data[i+j])) {
+                    return false;
+                }
+            }
+            if(bit != 0)
+                i += (bit-1);
+        }
+        //cout<<i<<endl;
+        return i == data.size();
+    }
+};
