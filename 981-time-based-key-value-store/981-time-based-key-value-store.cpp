e457810cc9b8723b411f29ba15913a5656a9dbf0
RakibLiar <liar.coder@gmail.com>
2022-10-07 02:36:01 +0600

    Time: 815 ms (13.45%), Space: 127.7 MB (98.95%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -15,7 +15,7 @@ public:
         if(mp1[key].size() == 0 || mp1[key][0] > timestamp) return "";
         int sz = mp1[key].size();
         if(mp1[key][sz-1] <= timestamp) return mp2[key][sz-1];
-        vector<int> v = mp1[key];
+        vector<int> &v = mp1[key];
         int index = lower_bound(v.begin(), v.end(), timestamp) - v.begin();
         if(v[index] > timestamp) index--;
         return mp2[key][index];
