fdb64a772a1ceae1efc1c8fba4be4a000d2d1a86
RakibLiar <liar.coder@gmail.com>
2024-01-15 18:23:26 +0600

    Time: 499 ms (66.32%), Space: 181.1 MB (39.23%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        vector<int> player(100001, 0), oneloss, zeroloss;
-        vector<bool> vis(100001, false);
-        for(vector<int> v: matches) {
-            player[v[1]]++;
-            vis[v[0]] = vis[v[1]] = true;
+        vector<int> oneloss, zeroloss;
+        unordered_map<int, int> mp;
+        
+        for(auto v: matches) {
+            if(!mp.count(v[0])) mp[v[0]] = 0;
+            mp[v[1]]++;
         }
-        for(int i=1; i<=100000; i++) {
-            if(vis[i]) {
-                if(player[i] == 0) {
-                    zeroloss.push_back(i);
-                } else if(player[i] == 1) {
-                    oneloss.push_back(i);
-                }
-            }
+        
+        for(auto it: mp) {
+            if(it.second == 0) zeroloss.push_back(it.first);
+            if(it.second == 1) oneloss.push_back(it.first);
         }
+        sort(oneloss.begin(), oneloss.end());
+        sort(zeroloss.begin(), zeroloss.end());
+        
         return {zeroloss, oneloss};
     }
 };
