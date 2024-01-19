1b112e5fb5697ba65cd3b4042610cc51465d13fc
RakibLiar <liar.coder@gmail.com>
2024-01-19 16:33:55 +0600

    Time: 12 ms (68.79%), Space: 13.5 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-
-    /*int func(int r , int c, vector<vector<int>>& matrix) {
-        if(r>= n || c>= m || r<0 || c < 0)return 1e9;
-        
-        if(r == n-1)return matrix[r][c];
-        
-        if(dp[r][c]!=-1e9)return dp[r][c];
-        
-        int leftDown = func(r + 1 , c - 1 , matrix);
-        int rightDown = func(r+1 , c + 1, matrix);
-        
-        int straightDown = func(r+1,c ,matrix);
-        
-        return dp[r][c] = matrix[r][c] + min(leftDown, min(rightDown, straightDown));
-    }*/
-    
     bool check(int n, int r, int c) {
         return 0 <= r && r < n && 0 <= c && c < n;
     }
@@ -26,14 +10,10 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
         for(int i=0; i<n-1; i++) {
             for(int j=0; j<n; j++) {
-                if(check(n, i+1, j+1)) {
-                    tempMat[i+1][j+1] = dp[i+1][j+1] = min(dp[i+1][j+1], tempMat[i][j] + mat[i+1][j+1]);
-                }
-                if(check(n, i+1, j)) {
-                    tempMat[i+1][j] = dp[i+1][j] = min(dp[i+1][j], tempMat[i][j] + mat[i+1][j]);
-                }
-                if(check(n, i+1, j-1)) {
-                    tempMat[i+1][j-1] = dp[i+1][j-1] = min(dp[i+1][j-1], tempMat[i][j] + mat[i+1][j-1]);
+                for(int k=-1; k<=1; k++) {
+                    if(check(n, i+1, j+k)) {
+                        tempMat[i+1][j+k] = dp[i+1][j+k] = min(dp[i+1][j+k], tempMat[i][j] + mat[i+1][j+k]);
+                    }
                 }
             }
         }
