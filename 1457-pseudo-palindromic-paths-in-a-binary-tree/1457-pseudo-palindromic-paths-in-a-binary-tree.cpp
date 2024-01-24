d38771c5d77e871e5bf973f91d53a74505db8645
RakibLiar <liar.coder@gmail.com>
2024-01-24 10:24:51 +0600

    Time: 250 ms (85.52%), Space: 177.7 MB (98.93%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -15,16 +15,13 @@ public:
         int c = 0;
         for(int a: v) {
             c += (a%2);
-            //cout<<a<<" ";
         }
-        //cout<<endl;
         return c <= 1;
     }
     int calculate(TreeNode *root, vector<int> &v) {
         if(root == nullptr) return 0;
         v[root->val]++;
         if(root->left == nullptr && root->right == nullptr) {
-            //cout<<root->val<<endl;
             int a = count(v);
             v[root->val]--;
             return a;
