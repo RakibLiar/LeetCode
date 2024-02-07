e105cd770729074db29c7204c103e5e8290a4bfc
RakibLiar <liar.coder@gmail.com>
2024-02-07 12:35:43 +0600

    Time: 11 ms (59.47%), Space: 9.6 MB (39.12%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        
-        vector < char > v;
-        vector < pair < int , char >> charCnt(256);
+        vector < pair < int , char >> charCnt(62);
         
         for(char &c: s) {
-            charCnt[c].first++;
-            charCnt[c].second = c;
+            if(c >= 'a' && c <= 'z') {
+                charCnt[c-'a'].first++;
+                charCnt[c-'a'].second = c;
+            } else if(c >= 'A' && c <= 'Z') {
+                charCnt[c-'A' + 26].first++;
+                charCnt[c-'A' + 26].second = c;
+            } else {
+                charCnt[c-'0' + 52].first++;
+                charCnt[c-'0' + 52].second = c;
+            }
         }
         
         sort(charCnt.begin(), charCnt.end(), [&](pair<int, char> &a, pair<int, char> &b) {
             return a.first > b.first;
         });
+        
         string ans = "";
         int a = -1;
         
-        while(charCnt[++a].first != 0) {
+        while(a < 61 && charCnt[++a].first != 0) {
             for(int i=0; i<charCnt[a].first; i++) {
                 ans.push_back(charCnt[a].second);
             }
