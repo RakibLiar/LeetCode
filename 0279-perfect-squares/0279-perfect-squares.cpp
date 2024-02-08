8e7e590bc7e5f565dc2c712bf362f67470e82cc9
RakibLiar <liar.coder@gmail.com>
2024-02-08 12:42:07 +0600

    Time: 41 ms (90.09%), Space: 10.5 MB (38.33%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> v(n+1, n+1);
-        v[0] = 0;
+        vector<int> dp(n+1, n+1);
+        dp[0] = 0;
         for(int i=1; i<=n; i++) {
             for(int j=1; j*j<=i; j++) {
-                v[i] = min(v[i], v[i-j*j] + 1);
+                dp[i] = min(dp[i], dp[i-j*j] + 1);
             }
         }
-        return v[n];
+        return dp[n];
     }
 };
