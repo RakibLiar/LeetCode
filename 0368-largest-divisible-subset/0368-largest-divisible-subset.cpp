71b008ff000346d411a29154b1888859d79a9c2c
RakibLiar <liar.coder@gmail.com>
2024-02-09 13:03:34 +0600

    Time: 25 ms (65.55%), Space: 15.9 MB (5.01%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void findLength(vector<vector<int>> &graph, vector<int> &level, int currentNode, vector<int> &path, vector<int> &longestPath, int d) {
+        //cout<<d<<" "<<currentNode<<" "<< level[currentNode]<<endl;
+        if(d <= level[currentNode]) return;
+        level[currentNode] = d;
+        path.push_back(currentNode);
+        if(path.size() > longestPath.size()) {
+            longestPath = path;
+        }
+        for(int child: graph[currentNode]) {
+            findLength(graph, level, child, path, longestPath, d + 1);
+        }
+        path.pop_back();
+        return;
+    }
+    
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        vector<vector<int>> graph(nums.size());
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                if(nums[j]%nums[i] == 0) {
+                    graph[i].push_back(j);
+                }
+            }
+        }
+        vector<int> level(nums.size(), 0);
+        vector<int> path, longestPath, res;
+        for(int i=0; i<nums.size(); i++) {
+            if(level[i] == 0) {
+                findLength(graph, level, i, path, longestPath, 1);
+            }
+        }
+        for(int &i: longestPath) {
+            res.push_back(nums[i]);
+        }
+        return res;
+    }
+};
