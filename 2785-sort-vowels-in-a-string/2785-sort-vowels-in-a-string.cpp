fbe1a4445d647ecb6ac5c4a34bc62da0b65fb837
RakibLiar <liar.coder@gmail.com>
2024-02-12 16:46:22 +0600

    Time: 18 ms (99.06%), Space: 13.8 MB (57.45%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
+    string vowelString = "AEIOUaeiou";
+    
     bool isVowel(char c) {
         return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
     
     char getVowel(int a) {
-        switch(a) {
-            case 0:
-                return 'A';
-            case 1:
-                return 'E';
-            case 2:
-                return 'I';
-            case 3:
-                return 'O';
-            case 4:
-                return 'U';
-            case 5:
-                return 'a';
-            case 6:
-                return 'e';
-            case 7:
-                return 'i';
-            case 8:
-                return 'o';
-            case 9:
-                return 'u';
-        }
-        return 0;
+        return vowelString[a];
     }
     
     char getVowelIndex(char c, vector<int> &v) {
@@ -41,29 +21,7 @@ public:
     }
     
     int vowelIndex(char c) {
-        switch(c) {
-            case 'A':
-                return 0;
-            case 'E':
-                return 1;
-            case 'I':
-                return 2;
-            case 'O':
-                return 3;
-            case 'U':
-                return 4;
-            case 'a':
-                return 5;
-            case 'e':
-                return 6;
-            case 'i':
-                return 7;
-            case 'o':
-                return 8;
-            case 'u':
-                return 9;
-        }
-        return 0;
+        return vowelString.find(c);
     }
     
     string sortVowels(string s) {
