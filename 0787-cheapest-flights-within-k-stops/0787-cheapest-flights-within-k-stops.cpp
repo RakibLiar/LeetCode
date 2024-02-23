75b5273b2e3df807ba4a2dcdf170510b55128813
RakibLiar <liar.coder@gmail.com>
2024-02-24 02:16:05 +0600

    Time: 31 ms (18.09%), Space: 17.5 MB (15.29%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>> graph(n), visited(n, vector<int>(k+2, INT_MAX));
-        vector<vector<int>> value(n, vector<int>(n, 0));
+        vector<vector<pair<int, int>>> graph(n);
+        vector<vector<int>> distance(n, vector<int>(k+2, INT_MAX));
         queue<int> q;
         int level = 0;
         q.push(src);
         for(vector<int> v: flights) {
-            graph[v[0]].push_back(v[1]);
-            value[v[0]][v[1]] = v[2];
+            graph[v[0]].push_back({v[1], v[2]});
         }
         for(int i=0; i<=k; i++) {
-            visited[src][0] = 0;
+            distance[src][0] = 0;
         }
         
         while(level <= k && !q.empty()) {
@@ -19,10 +18,11 @@ public:
             while(sz--) {
                 int parent = q.front();
                 q.pop();
-                for(int child: graph[parent]) {
-                    //cout<<parent<<" "<<child<<" "<<level<<endl;
-                    if(visited[child][level+1] > visited[parent][level] + value[parent][child]) {
-                        visited[child][level+1] = visited[parent][level] + value[parent][child];
+                for(pair<int, int> &childPair: graph[parent]) {
+                    int child = childPair.first;
+                    int value = childPair.second;
+                    if(distance[child][level+1] > distance[parent][level] + value) {
+                        distance[child][level+1] = distance[parent][level] + value;
                         q.push(child);
                     }
                 }
@@ -31,7 +31,7 @@ public:
         }
         int res = INT_MAX;
         for(int i=0; i<=level; i++) {
-            res = min(res, visited[dst][i]);
+            res = min(res, distance[dst][i]);
         }
         return res == INT_MAX ? -1 : res;
     }
