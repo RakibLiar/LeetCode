9307729475e03e05948afed7834516aa841e5972
RakibLiar <liar.coder@gmail.com>
2024-03-30 05:02:29 +0600

    Time: 160 ms (27.93%), Space: 125.2 MB (25.00%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -3,10 +3,12 @@ public:
     long long countSubarrays(vector<int>& nums, int k) {
         vector<int> cnt(nums.size(), 0);
         long long ans = 0, c = 0, mx = *max_element(nums.begin(), nums.end());
-        cnt[0] = (nums[0] == mx);
-        c = (nums[0] == mx);
-        if(nums[0] == mx && k == 1)
-            ans = 1;
+        if(nums[0] == mx) {
+            cnt[0] = 1;
+            c = 1;
+            if(k == 1)
+                ans = 1;
+        }
         for(int i=1; i<nums.size(); i++) {
             int a = nums[i];
             if(a == mx) {
@@ -18,11 +20,9 @@ public:
             
             if(c >= k) {
                 int ind = lower_bound(cnt.begin(), cnt.begin() + i, c-k+1) - cnt.begin();
-                //cout<<ind<<" "<<i<<" "<<c<<" "<<k<<endl;
                 ans += (ind+1);
             }
         }
-        //for(int a: cnt) cout<<a<<" ";
         return ans;
     }
 };
