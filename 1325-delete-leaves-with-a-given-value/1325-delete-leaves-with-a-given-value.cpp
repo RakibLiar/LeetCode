aa3cbbaf8449ef33255db44e234c94f66b484c6d
RakibLiar <liar.coder@gmail.com>
2024-05-17 13:19:29 +0600

    Time: 7 ms (92.90%), Space: 21.2 MB (47.43%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -13,6 +13,10 @@ class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
         if(root == nullptr) return root;
+        if(root->left == nullptr && root->right == nullptr) {
+            if(root->val == target) return nullptr;
+            return root;
+        }
         root->left = removeLeafNodes(root->left, target);
         root->right = removeLeafNodes(root->right, target);
         if(root->left == nullptr && root->right == nullptr && root->val == target) return nullptr;
