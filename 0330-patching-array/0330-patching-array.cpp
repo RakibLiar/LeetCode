eef04a66c6d4c3e766df8f1ea6559af0cf84b0e1
RakibLiar <liar.coder@gmail.com>
2024-06-18 03:34:50 +0600

    Time: 3 ms (84.45%), Space: 13.8 MB (8.88%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int i = 0, res=0;
+        while(miss <= n) {
+            if(i < nums.size() && miss >= nums[i]) {
+                miss += nums[i++];
+            } else {
+                miss <<= 1;
+                res++;
+            }
+        }
+        return res;
+    }
+};
