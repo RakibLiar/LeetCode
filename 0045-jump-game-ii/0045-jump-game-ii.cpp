07ec20f7289bd914a8428aedc387ef698c1bd679
RakibLiar <liar.coder@gmail.com>
2024-07-09 02:26:19 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int MX = 100000;
-    int calculateResult(vector<int> &nums, vector<int> &dp, int pos) {
-        if(pos == nums.size() - 1) return 0;
-        if(dp[pos] != MX) return dp[pos];
-        
-        for(int i=pos+1; i <= pos + nums[pos] && i < nums.size(); i++) {
-            dp[pos] = min(dp[pos], 1 + calculateResult(nums, dp, i));
-        }
-        return dp[pos];
-    }
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), MX);
-        return calculateResult(nums, dp, 0);
+        int currentMax = 0, nextMax = 0, jumps = 0;
+        for(int i=0; i<nums.size()-1; i++) {
+            nextMax = max(nextMax, i + nums[i]);
+            if(i == currentMax) {
+                currentMax = nextMax;
+                jumps++;
+                if(currentMax >= nums.size() - 1) return jumps;
+            }
+        }
+        return -1;
     }
 };
