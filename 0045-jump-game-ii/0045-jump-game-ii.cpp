3fea22511f618d859a59931c7735ba361d60b33d
RakibLiar <liar.coder@gmail.com>
2024-07-09 02:22:02 +0600

    Time: 307 ms (5.03%), Space: 20.6 MB (5.06%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int MX = 100000;
+    int calculateResult(vector<int> &nums, vector<int> &dp, int pos) {
+        if(pos == nums.size() - 1) return 0;
+        if(dp[pos] != MX) return dp[pos];
+        
+        for(int i=pos+1; i <= pos + nums[pos] && i < nums.size(); i++) {
+            int res = calculateResult(nums, dp, i);
+            dp[pos] = min(dp[pos], 1 + res);
+        }
+        return dp[pos];
+    }
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), MX);
+        return calculateResult(nums, dp, 0);
+    }
+};
