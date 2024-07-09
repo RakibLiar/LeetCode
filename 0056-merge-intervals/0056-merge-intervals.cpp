5e1991ee3eb9f071b62fe8ed23a1061d301b517c
RakibLiar <liar.coder@gmail.com>
2024-07-10 03:24:04 +0600

    Time: 22 ms (68.11%), Space: 22.6 MB (67.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& v1, const vector<int>& v2){
+            if(v1[0] == v2[0]) return v1[1] < v2[1];
+            return v1[0] < v2[0];
+        });
         vector<vector<int>> result;
         int start = intervals[0][0], end = intervals[0][1];
         for(vector<int> &v: intervals) {
