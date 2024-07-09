c0818a62c7120940d0df044a282c1d58c51ec18f
RakibLiar <liar.coder@gmail.com>
2024-07-10 03:23:02 +0600

    Time: 21 ms (73.94%), Space: 22.8 MB (41.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,7 +4,7 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> result;
         int start = intervals[0][0], end = intervals[0][1];
-        for(vector<int> v: intervals) {
+        for(vector<int> &v: intervals) {
             if(v[0] > end) {
                 result.push_back({start, end});
                 start = v[0];
