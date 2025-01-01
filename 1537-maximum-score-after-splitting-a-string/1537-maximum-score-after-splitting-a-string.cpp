fcfa881c51e020ae3c3b321ba327eda3b5da3e12
RakibLiar <liar.coder@gmail.com>
2025-01-02 00:40:41 +0600

    Time: 0 ms (100%), Space: 8.7 MB (52.6%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int tot0 = 0, tot1 = 0, cur0 = 0, cur1 = 0, ans = 0;
+        int tot1 = 0, cur0 = 0, cur1 = 0, ans = 0;
         for(char c: s) {
-            if(c == '0') tot0++;
-            else tot1++;
+            if(c == '1') tot1++;
         }
         for(int i=0; i<s.size()-1; i++) {
             if(s[i] == '0') cur0++;
