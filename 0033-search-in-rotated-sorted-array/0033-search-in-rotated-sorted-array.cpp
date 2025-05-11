6d2e8640407b768651ddbd25fd1e5ec737f30926
RakibLiar <liar.coder@gmail.com>
2025-05-11 23:44:27 +0600

    Time: 0 ms (100%), Space: 15.3 MB (33.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int findSeparatorIndex(vector<int> &nums) {
+    int getMidIndex(vector<int> &nums) {
+        if(nums.size() == 1) return 0;
         int n = nums.size()-1;
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n] < nums[n-1]) return n-1;
-        int low = 1, high = n-1, index = 0;
-        while(low < high) {
-            int mid = (low + high) / 2, index = 0;
-            if(nums[mid] > nums[mid+1]) return mid;
-            if(nums[mid] > nums[low]) {
-                low = mid;
-            } else {
-                high = mid;
+        if(nums[0] < nums[n]) return 0;
+        int st = 1, end = n-1, mid;
+        while(st <= end) {
+            mid = (st + end) / 2;
+            if(nums[mid] > nums[mid+1]) {
+                mid++;
+                break;
             }
+            if(nums[mid] > nums[n]) st = mid + 1;
+            else end = mid - 1;
         }
-        return -1;
+        return mid;
     }
 
-    int findElement(vector<int> &nums, int low, int high, int target) {
-        while(low <= high) {
-            int mid = (low+high)/2;
+    int binarySearch(vector<int> &nums, int target, int start, int end) {
+        int mid;
+        while(start <= end) {
+            mid = (start + end) / 2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] < target) low = mid+1;
-            else high = mid-1;
+            if(nums[mid] > target) end = mid-1;
+            else start = mid + 1;
         }
         return -1;
     }
 
     int search(vector<int>& nums, int target) {
-        if(nums.size() == 1) {
-            if(nums[0] == target) return 0;
-            return -1;
-        }
-        int midIndex = findSeparatorIndex(nums);
+        int midIndex = getMidIndex(nums);
         cout<<midIndex<<endl;
-        int index = findElement(nums, 0, midIndex, target);
-        if(index != -1) return index;
-        index = findElement(nums, midIndex+1, nums.size()-1, target);
-        if(index != -1) return index;
+        auto ind = binarySearch(nums, target, 0, midIndex-1);
+        if(ind != -1) return ind;
+        ind = binarySearch(nums, target, midIndex, nums.size()-1);
+        if(ind != -1) return ind;
         return -1;
     }
 };
