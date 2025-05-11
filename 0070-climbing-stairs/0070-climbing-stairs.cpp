3ad5d7a66e968e147245339ba0165a18576830c3
RakibLiar <liar.coder@gmail.com>
2025-05-12 01:34:35 +0600

    Time: 0 ms (100%), Space: 7.8 MB (87.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    vector<int> dp;
-    int hululu(int n) {
-        if(n<=2) {
-            return n;
-        }
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = hululu(n-1) + hululu(n-2);
-    }
     int climbStairs(int n) {
-        dp = vector<int>(n+1, -1);
-        return hululu(n);
+        if(n == 1 || n == 2) return n;
+        int f1 = 1, f2 = 2, res;
+        for(int i=3; i<=n; i++) {
+            res = f1 + f2;
+            f1 = f2;
+            f2 = res;
+        }
+        return res;
     }
 };
