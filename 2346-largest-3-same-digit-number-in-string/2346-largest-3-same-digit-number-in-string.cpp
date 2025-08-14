0965f2dbeda098fd1aac06919ace3ecb20821657
RakibLiar <liar.coder@gmail.com>
2025-08-14 22:32:28 +0600

    Time: 39 ms (3.26%), Space: 8.9 MB (96.52%) - LeetHub

---
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+        for(int i=0; i<num.size()-2; i++) {
+            if(num[i] == num[i+1] && num[i+1] == num[i+2]) {
+                cout<<num[i]<<endl;
+                cout<<ans<<endl;
+                if(ans.size() == 0 || ans[0] < num[i]) ans = num.substr(i, 3);
+            }
+        }
+        return ans;
+    }
+};
