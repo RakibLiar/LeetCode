0d9a971e9ccb38b783c6ec066ff467a90e40e8e7
RakibLiar <liar.coder@gmail.com>
2025-08-18 00:24:50 +0600

    Time: 0 ms (100%), Space: 7.8 MB (85.56%) - LeetHub

--- 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int smallestRepunitDivByK(int k) {
         int num = 0, i;
-        unordered_map<int, bool> mp;
         for(i = 1; i<=k; i++) {
             num = (10 * num + 1) % k;
             if(num == 0) return i;
-            if(mp.count(num)) return -1;
-            mp[num] = true;
         }
         return -1;
     }
