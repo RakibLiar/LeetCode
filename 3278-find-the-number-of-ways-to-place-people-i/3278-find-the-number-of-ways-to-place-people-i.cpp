fd0d99ce7c54b83eb0d5b3b404e628eb556fe15b
RakibLiar <liar.coder@gmail.com>
2025-09-02 23:24:00 +0600

    Time: 0 ms (100%), Space: 32.7 MB (39.26%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    bool isInside(vector<int> &left, vector<int> &right, vector<int> &mid) {
-        return left[1] >= mid[1] && mid[1] >= right[1];
-    }
-
-    bool isLowerRight(vector<int> &left, vector<int> &right) {
-        return left[1] >= right[1];
-    }
-
     int numberOfPairs(vector<vector<int>>& points) {
         int cnt = 0;
         sort(points.begin(), points.end(), [&](vector<int> &a, vector<int> &b) {
-            if(a[0] != b[0]) return a[0] < b[0];
-            return a[1] > b[1];
+            return a[0] != b[0] ? a[0] < b[0] : a[1] > b[1];
         });
 
         for(int i=0; i<points.size(); i++) {
+            int lastY = -1;
             for(int j=i+1; j<points.size(); j++) {
-                if(points[i][1] >= points[j][1]) {
-                    int k;
-                    for(k=i+1; k<j; k++) {
-                        if(isInside(points[i], points[j], points[k])) {
-                            break;
-                        }
-                    }
-                    if(k == j) cnt++;
+                if(points[i][1] >= points[j][1] &&  points[j][1] > lastY) {
+                    lastY = points[j][1];
+                    cnt++;
                 }
             }
         }
