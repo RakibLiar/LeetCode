f52eaf6c43d9b5ebd78e2c2eefddaf3d25dd348a
RakibLiar <liar.coder@gmail.com>
2026-02-12 01:36:36 +0600

    Time: 122 ms (12.72%), Space: 186.7 MB (14.4%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -63,10 +63,3 @@ public:
         mp[key] = head->next;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
